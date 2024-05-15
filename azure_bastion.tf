37585d37d94fd724c54f4467931f20a62aeb4cfc
Kazuhiro Umemura <supernovatypebiz@gmail.com>
2024-05-15 17:47:39 +0900

    add bastion

--- azure_bastion.tf
+++ azure_bastion.tf
@@ -18,7 +18,7 @@ resource "azurerm_network_security_group" "bastion_nsg" {
     priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
     source_address_prefix      = "GatewayManager"
@@ -30,7 +30,7 @@ resource "azurerm_network_security_group" "bastion_nsg" {
     priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
     source_address_prefix      = "Internet"
@@ -42,7 +42,7 @@ resource "azurerm_network_security_group" "bastion_nsg" {
     priority                   = 1001
     direction                  = "Outbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_ranges     = ["3389","22"]
     source_address_prefix      = "*"
@@ -54,7 +54,7 @@ resource "azurerm_network_security_group" "bastion_nsg" {
     priority                   = 1002
     direction                  = "Outbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
     source_address_prefix      = "*"
@@ -79,7 +79,7 @@ resource "azurerm_bastion_host" "bastion" {
   name                = "bastion"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku                 = Standard
+  sku                 = "Standard"
   tunneling_enabled   = true
 
   ip_configuration {
